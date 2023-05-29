d8b5bf15c937cfecf3fee668a944f3c559eba7d0
Gabriel <season8jason@gmail.com>
2023-05-29 12:54:01 +0200

    Latest patch

--- frontend/views/posts/index.php
+++ frontend/views/posts/index.php
@@ -1,22 +1,20 @@
 <?php
 require_once __DIR__ . "/../../Template.php";
 
-Template::header("Purchases");
+Template::header("Posts");
 ?>
 
-<h1>Purchases</h1>
+<h1>Post</h1>
 
-<a href="<?= $this->home ?>/purchases/new">Create new</a>
+<a href="<?= $this->home ?>/posts/new">Create new</a>
 
 <div class="item-grid">
 
-    <?php foreach ($this->model as $purchase) : ?>
+    <?php foreach ($this->model as $post) : ?>
 
         <article class="item">
             <div>
-                <b><?= $purchase->product_name ?></b> <br>
-                <span>Price: <?= $purchase->price ?></span> <br>
-                <span>Purchased: <?= $purchase->purchase_time ?></span> <br>
+                <b><?= $post->content ?></b> <br>
             </div>
 
 
@@ -24,13 +22,13 @@ Template::header("Purchases");
 
                 <p>
                     <b>User ID: </b>
-                    <?= $purchase->user_id ?>
+                    <?= $post->user_id ?>
                 </p>
-            <a href="<?= $this->home ?>/purchases/<?= $purchase->purchase_id ?>/edit">Edit</a>
+            <a href="<?= $this->home ?>/posts/<?= $post->post_id ?>/edit">Edit</a>
 
             <?php endif; ?>
 
-            <a href="<?= $this->home ?>/purchases/<?= $purchase->purchase_id ?>">Show</a>
+            <a href="<?= $this->home ?>/posts/<?= $post->post_id ?>">Show</a>
         </article>
 
     <?php endforeach; ?>
